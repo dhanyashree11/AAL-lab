570424533c403364490c99335c891fcb5ea315b7
dhanyashree11 <dhanyashrees.ai23@bmsce.ac.in>
2025-05-16 10:13:41 +0530

    Lab 3A Merge Sort.c

--- Lab 3A Merge Sort.c
+++ Lab 3A Merge Sort.c
@@ -1,43 +1,65 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int merge(left,right){
-	int a=len(left);
-	int b=len(right);
-	int n=a+b;
-	int arr[n];
-	int i=0,j=0,k=0;
-	while (i<a && j<b){
-		if (left[i]<right[j]){
-			arr[k]=left[i];
-			i++;
-			k++;
-		}
-		else{
-			arr[k]=right[j];
-			j++;
-			k++;
-		}
-	}
-	int x,y;
-	for (x=i;x<len(left);x++){
-		arr[k]=left[x];
-		k++;
-	}	
-	for (y=j;y<len(right);y++){
-		arr[k]=right[y];
-		k++;
-	}
+void merge(int arr[], int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1; 
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+        }
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+
+    while (j < n2)
+        arr[k++] = R[j++];
 }
-int merge_sort(arr){
-	if (len(arr)<=1)
-		return arr;
-	mid=int(arr/2);
-	sortedleft=merge_sort(arr[:mid]);
-	sortedright=merge_sort(arr[mid:]);
-	return merge(sortedleft,sortedright);
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
 }
-int main(int argc, char *argv[]) {
-	
-	return 0;
+
+void main() {
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    mergeSort(arr, 0, n - 1);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
 }
+
+OUTPUT:
+Original array:
+38 27 43 3 9 82 10 
+Sorted array:
+3 9 10 27 38 43 82 
