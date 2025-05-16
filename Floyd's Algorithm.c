37a6e99d6717bd1d882b9e53502d9b2fb4fa08fb
dhanyashree11 <dhanyashrees.ai23@bmsce.ac.in>
2025-05-16 09:08:07 +0530

    Floyd's Algorithm.c

---
+++ Floyd's Algorithm.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define V 10  
+#define INF 99999 
+void printMatrix(int dist[][V], int n);
+void floydWarshall(int graph[][V], int n) {
+    int dist[V][V];
+    int i, j, k;
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            dist[i][j] = graph[i][j];
+    for (k = 0; k < n; k++) {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+    printf("Shortest distances between every pair of vertices:\n");
+    printMatrix(dist, n);
+}
+
+void printMatrix(int dist[][V], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n, i, j;
+    int graph[V][V];
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+    printf("Enter adjacency matrix (use %d for INF):\n", INF);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+    floydWarshall(graph, n);
+    return 0;
+}
